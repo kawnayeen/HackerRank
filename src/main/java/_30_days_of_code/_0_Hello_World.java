62cead8dc9a7143c748f6edcecad7dd4e3f952df
kawnayeen <kawnayeen.08@gmail.com>
2018-08-26 22:35:11 +0600

    [30 days of code] day 0 - Hello, World! solved

---
+++ src/main/java/_30_days_of_code/_0_Hello_World.java
@@ -0,0 +1,8 @@
+package _30_days_of_code;
+
+public class _0_Hello_World {
+    public void printOutput(String input) {
+        System.out.println("Hello, World.");
+        System.out.println(input);
+    }
+}
---
+++ src/test/java/_30_days_of_code/_0_Hello_WorldTest.java
@@ -0,0 +1,36 @@
+package _30_days_of_code;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class _0_Hello_WorldTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    public void testConsoleOutput() {
+        _0_Hello_World helloWorld = new _0_Hello_World();
+        helloWorld.printOutput("Kamarul Kawnayeen");
+        assertEquals("Hello, World.\nKamarul Kawnayeen\n", outContent.toString());
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+}
