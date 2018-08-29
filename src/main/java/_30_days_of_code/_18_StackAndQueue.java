c8c9e6a7ebb8535d9f2e02fdc0721f758635fa36
kawnayeen <kawnayeen.08@gmail.com>
2018-08-29 22:31:49 +0600

    [30 days of code] day 18 - stack and queue solved - need to refactor

--- src/main/java/_30_days_of_code/_18_StackAndQueue.java
+++ src/main/java/_30_days_of_code/_18_StackAndQueue.java
@@ -1,8 +1,36 @@
 package _30_days_of_code;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class _18_StackAndQueue {
+    private Stack<Character> stack;
+    private Queue<Character> queue;
+
+    public _18_StackAndQueue() {
+        stack = new Stack<>();
+        queue = new LinkedList<>();
+    }
+
     public boolean isPalindrome(String input) {
         String reversed = new StringBuilder(input).reverse().toString();
         return input.equals(reversed);
+    }
+
+    public void pushCharacter(Character character){
+        stack.push(character);
+    }
+
+    public void enqueueCharacter(Character character){
+        queue.add(character);
+    }
+
+    public Character popCharacter(){
+        return stack.pop();
+    }
+
+    public Character dequeueCharacter() {
+        return queue.remove();
     }
 }
