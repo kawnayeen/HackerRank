46b145f95377d2a145c61b194cd4c765f1a44745
kawnayeen <kawnayeen.08@gmail.com>
2018-09-04 17:09:16 +0600

    [Algorithm] Staircase solved

---
+++ src/main/java/algorithm/_6_Staircase.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+/**
+ * kawnayeen
+ * 4/9/18
+ */
+public class _6_Staircase {
+    private static final String SPACE = " ";
+    private static final String HASH = "#";
+
+    String stairCase(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < (n - i - 1))
+                    stringBuilder.append(SPACE);
+                else
+                    stringBuilder.append(HASH);
+            }
+            if (i + 1 != n)
+                stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/java/algorithm/_6_StaircaseTest.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * kawnayeen
+ * 4/9/18
+ */
+public class _6_StaircaseTest {
+    @Test
+    public void testStairCase() {
+        _6_Staircase staircase = new _6_Staircase();
+        System.out.print(staircase.stairCase(4));
+    }
+}
