6f8d978d2d4111a0a8a390aa209cb8661f4f6122
kawnayeen <kawnayeen.08@gmail.com>
2018-09-04 16:42:49 +0600

    [Algorithm] PlusMinus fraction solved

---
+++ src/main/java/algorithm/_5_PlusMinus.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+/**
+ * kawnayeen
+ * 4/9/18
+ */
+public class _5_PlusMinus {
+    void plusMinus(int[] arr) {
+        int numberOfPositiveValue = 0;
+        int numberOfZero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0)
+                numberOfZero++;
+            else if (arr[i] > 0)
+                numberOfPositiveValue++;
+        }
+        double positiveFraction = (numberOfPositiveValue * 1f) / arr.length;
+        double zeroFraction = (numberOfZero * 1f) / arr.length;
+        double negativeFraction = ((arr.length - numberOfPositiveValue - numberOfZero) * 1f) / arr.length;
+        System.out.println(positiveFraction);
+        System.out.println(negativeFraction);
+        System.out.println(zeroFraction);
+    }
+}
