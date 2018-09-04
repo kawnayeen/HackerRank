e0b81aa82f46e04f35245329042fe5bf307187b1
kawnayeen <kawnayeen.08@gmail.com>
2018-09-04 20:43:52 +0600

    [Algorithm] Time Conversion solved

---
+++ src/main/java/algorithm/_10_TimeConversion.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+public class _10_TimeConversion {
+    private static final String AM = "AM";
+    private static final String PM = "PM";
+    private static final String EMPTY_STRING = "";
+
+    public String timeConversion(String s) {
+        int hour = getHour(s);
+        if (s.endsWith(AM)) {
+            s = s.replace(AM, EMPTY_STRING);
+            return adjustAM(hour, s);
+        }
+        s = s.replace(PM, EMPTY_STRING);
+        return adjustPM(hour, s);
+    }
+
+    private int getHour(String s) {
+        return Integer.parseInt(s.substring(0, 2));
+    }
+
+    private String adjustAM(int hour, String time) {
+        if (hour == 12)
+            return "00" + time.substring(2);
+        return time;
+    }
+
+    private String adjustPM(int hour, String time) {
+        if (hour == 12)
+            return time;
+        else
+            return (hour + 12) + time.substring(2);
+    }
+}
---
+++ src/test/java/algorithm/_10_TimeConversionTest.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class _10_TimeConversionTest {
+    @Test
+    public void testTimeConversion() {
+        _10_TimeConversion timeConversion = new _10_TimeConversion();
+        Assert.assertEquals("19:05:45",timeConversion.timeConversion("07:05:45PM"));
+        Assert.assertEquals("00:40:22",timeConversion.timeConversion("12:40:22AM"));
+        Assert.assertEquals("12:40:22",timeConversion.timeConversion("12:40:22PM"));
+    }
+}
