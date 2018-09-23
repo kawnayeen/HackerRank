032e3e63efac10c0d7cd9300e623e570f5551456
kawnayeen <kawnayeen.08@gmail.com>
2018-09-23 18:54:33 +0600

    [10 days of statistics] day 5 - normal distribution I solved

--- src/main/java/_10_days_of_statistics/_5_NormalDistribution.java
+++ src/main/java/_10_days_of_statistics/_5_NormalDistribution.java
@@ -1,4 +1,13 @@
 package _10_days_of_statistics;
 
+import util.DoubleUtil;
+import util.ErrorFunction;
+
 public class _5_NormalDistribution {
+    double getCumulativeProbability(double upTo, double mean, double standardDeviation) {
+        double z = (upTo - mean) / (standardDeviation * Math.sqrt(2));
+        double zScore = ErrorFunction.erf(z);
+        double cumulativeProbability = 0.5 * (1 + zScore);
+        return DoubleUtil.round(cumulativeProbability, 3);
+    }
 }
