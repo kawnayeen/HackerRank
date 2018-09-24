a6eb6daf8714e32c9b8b744c23b8e75900a73f91
kawnayeen <kawnayeen.08@gmail.com>
2018-09-24 22:40:26 +0600

    [Algorithm] String - 'Sherlock and Valid String' solution refactored

--- src/main/java/algorithm/string/_16_SherlockAndValidString.java
+++ src/main/java/algorithm/string/_16_SherlockAndValidString.java
@@ -1,7 +1,6 @@
 package algorithm.string;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class _16_SherlockAndValidString {
     private static final String YES = "YES";
@@ -23,44 +22,23 @@ public class _16_SherlockAndValidString {
             return YES;
         if (charSet.size() == s.length())
             return YES;
-        HashMap<Integer, Integer> fMap = new HashMap<>();
-        for (char ch : charSet) {
-            int occurrence = charFrequency.get(ch);
-            if (fMap.containsKey(occurrence)) {
-                int count = fMap.get(occurrence);
-                count++;
-                fMap.put(occurrence, count);
-            } else {
-                fMap.put(occurrence, 1);
-            }
-        }
 
-        Set<Integer> valSet = fMap.keySet();
-        if (valSet.size() > 2)
-            return NO;
-        if (valSet.size() == 1)
-            return YES;
-        int firstVal = -1;
-        int firstValCount = -1;
-        int secondVal = -1;
-        int secondValCount = -1;
-        for (int val : valSet) {
-            if (firstVal == -1) {
-                firstVal = val;
-                firstValCount = fMap.get(val);
-            } else if (secondVal == -1) {
-                secondVal = val;
-                secondValCount = fMap.get(val);
+        List<Integer> frequencyList = new ArrayList<>(charFrequency.values());
+        Collections.sort(frequencyList);
+        int startIndex = 0;
+        int delCount = 0;
+        String isValid = YES;
+        if (frequencyList.get(0) == 1 && frequencyList.get(1) > 1) {
+            startIndex = 1;
+            delCount = 1;
+        }
+        for (int i = startIndex; i < frequencyList.size() - 1; i++) {
+            delCount += frequencyList.get(i + 1) - frequencyList.get(startIndex);
+            if (delCount > 1) {
+                isValid = NO;
+                break;
             }
         }
-        if (firstVal == 1 && firstValCount == 1)
-            return YES;
-        if (secondVal == 1 && secondValCount == 1)
-            return YES;
-        if (firstValCount == 1 && firstVal - secondVal == 1)
-            return YES;
-        if (secondValCount == 1 && secondVal - firstVal == 1)
-            return YES;
-        return NO;
+        return isValid;
     }
 }
--- src/test/java/algorithm/string/_16_SherlockAndValidStringTest.java
+++ src/test/java/algorithm/string/_16_SherlockAndValidStringTest.java
@@ -1,18 +1,19 @@
 package algorithm.string;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class _16_SherlockAndValidStringTest {
     @Test
     public void testValidString() {
         _16_SherlockAndValidString validString = new _16_SherlockAndValidString();
-        System.out.println(validString.isValid("abcdefghhgfedecba"));
-        System.out.println(validString.isValid("aabbccddeefghi"));
-        System.out.println(validString.isValid("aabbcd"));
-        System.out.println(validString.isValid("aabbc"));
-        System.out.println(validString.isValid("abcc"));
-        System.out.println(validString.isValid("aaabbbbcccc"));
-        System.out.println(validString.isValid("abbbbcccc"));
-        System.out.println(validString.isValid("abbbcccdddd"));
+        Assert.assertEquals("YES", validString.isValid("abcdefghhgfedecba"));
+        Assert.assertEquals("NO",validString.isValid("aabbccddeefghi"));
+        Assert.assertEquals("NO",validString.isValid("aabbcd"));
+        Assert.assertEquals("YES",validString.isValid("aabbc"));
+        Assert.assertEquals("YES",validString.isValid("abcc"));
+        Assert.assertEquals("NO",validString.isValid("aaabbbbcccc"));
+        Assert.assertEquals("YES",validString.isValid("abbbbcccc"));
+        Assert.assertEquals("NO",validString.isValid("abbbcccdddd"));
     }
 }
