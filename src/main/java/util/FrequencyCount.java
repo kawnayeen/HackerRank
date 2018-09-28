3502a9da9858492bf6a3f0d71eed1d568a094c61
kawnayeen <kawnayeen.08@gmail.com>
2018-09-28 22:18:55 +0600

    [Algorithm] String - 'Game of Thrones I' solved

--- src/main/java/util/FrequencyCount.java
+++ src/main/java/util/FrequencyCount.java
@@ -6,15 +6,23 @@ import java.util.Map;
 public class FrequencyCount {
     public static Map<Integer, Integer> countFrequency(int[] input) {
         Map<Integer, Integer> frequencyByNumber = new HashMap<>();
-        for(int val: input){
-            if(frequencyByNumber.containsKey(val)){
-                int presentCount = frequencyByNumber.get(val);
-                presentCount++;
-                frequencyByNumber.put(val,presentCount);
-            }else {
-                frequencyByNumber.put(val,1);
-            }
+        for (int val : input) {
+            if (frequencyByNumber.containsKey(val))
+                frequencyByNumber.put(val, frequencyByNumber.get(val) + 1);
+            else
+                frequencyByNumber.put(val, 1);
         }
         return frequencyByNumber;
+    }
+
+    public static Map<Character, Integer> countCharFrequency(String s) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            if (charCount.containsKey(ch))
+                charCount.put(ch, charCount.get(ch) + 1);
+            else
+                charCount.put(ch, 1);
+        }
+        return charCount;
     }
 }
