f9541df390115df00f1b53cd7ae1d690b17a81e6
kawnayeen <kawnayeen.08@gmail.com>
2018-10-02 10:11:07 +0600

    [Data Structures] Queue using stack updated

--- src/main/java/datastructure/queue/_1_QueueUsingStacks.java
+++ src/main/java/datastructure/queue/_1_QueueUsingStacks.java
@@ -11,27 +11,32 @@ public class _1_QueueUsingStacks {
         Stack<Integer> primaryStack = new Stack<>();
         Stack<Integer> backup = new Stack<>();
         StringBuilder builder = new StringBuilder();
-        int oldestVal = 0;
         for (String query : queries) {
             if (query.startsWith(ENQUEUE)) {
-                int val = Integer.parseInt(query.substring(2));
-                if (primaryStack.size() == 0)
-                    oldestVal = val;
-                primaryStack.push(val);
+                primaryStack.push(Integer.parseInt(query.substring(2)));
             } else if (query.startsWith(DEQUEUE)) {
-                backup.clear();
-                while (!primaryStack.isEmpty())
-                    backup.push(primaryStack.pop());
-                backup.pop();
+                // this is kind of tricky to avoid timeout error
+                // we are splitting the values in 2 stack
+                // backup will contain the oldest at top and newest at bottom
+                // when backup is fill up - primaryStack will be empty & we are not going to refill it
+                // instead when backup will empty again, we will refill it from primary stack
+                //
+                // so in single word, once dequeue is called, backup & primary stack will hold all the element combined
+                if (backup.isEmpty()) {
+                    while (!primaryStack.isEmpty())
+                        backup.push(primaryStack.pop());
+                }
                 if (!backup.isEmpty())
-                    oldestVal = backup.peek();
-                else
-                    oldestVal = 0;
-                while (!backup.empty())
-                    primaryStack.push(backup.pop());
+                    backup.pop();
             } else {
-                builder.append(oldestVal);
-                builder.append("\n");
+                if (backup.isEmpty()) {
+                    while (!primaryStack.isEmpty())
+                        backup.push(primaryStack.pop());
+                }
+                if (!backup.isEmpty()) {
+                    builder.append(backup.peek());
+                    builder.append("\n");
+                }
             }
         }
         System.out.println(builder.toString());
